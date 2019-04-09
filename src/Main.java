663c036f172534ad2a0eea2a926d8eea918f36a4
Skylar <skylarhimeno@gmail.com>
2019-04-09 09:53:38 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import javax.swing.JFrame;
 
 public class Main
 {
+	public Main()
+	{
+		JFrame frame = new JFrame();
+		Panel panel = new Panel();
+		
+		frame.add(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("SNAKECODING");
+		frame.setLocationRelativeTo(null);
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args)
+	{
+		new Main();
+	}
 
 }
