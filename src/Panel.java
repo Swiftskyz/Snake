be5c12c72739708dfb4d780f257a575b57cfe620
Skylar <skylarhimeno@gmail.com>
2019-04-09 09:30:18 -0600

    updated panel and created body

--- src/Panel.java
+++ src/Panel.java
@@ -1,5 +1,79 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
 
-public class Panel
+import javax.swing.JPanel;
+
+public class Panel extends JPanel implements Runnable
 {
+	private static final long serialVersionUID = 1L;
+	
+	public static final int WIDTH = 500, HEIGHT = 500;
+	
+	private Thread thread;
+	
+	private boolean running;
+	
+	public Panel()
+	{
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		
+	}
+	
+	public void start()
+	{
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	public void stop()
+	{
+		running = false;
+		try
+		{
+			thread.join();
+		}
+		catch (InterruptedException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void tick()
+	{
+		
+	}
+	
+	public void paint(Graphics g)
+	{
+		g.clearRect(0, 0, WIDTH, HEIGHT);
+		
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		
+		for(int i = 0 ; i < WIDTH/10 ; i++)
+		{
+			g.drawLine(i * 10, 0, i * 10, HEIGHT);
+		}
+		for(int i = 0 ; i < HEIGHT/10 ; i++)
+		{
+			g.drawLine(0, i * 10, HEIGHT, i * 10);
+		}
+	}
+
+	@Override
+	public void run()
+	{
+		while(running)
+		{
+			tick();
+			repaint();
+		}
+		
+	}
+	
+	
 
 }
